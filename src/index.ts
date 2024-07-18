6475b8a6ca40adf8e809c9ab69fbf82b43230622
Naico Wang <>
2024-07-18 09:13:16 +0800

    feat(update): change to TS version

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-const LOG_LEVEL = {
-  INFO: 'Info',
-  ERROR: 'Error',
-  WARNING: 'Warning',
-  SUCCESS: 'Success'
+enum LOG_LEVEL {
+  INFO = 'Info',
+  ERROR = 'Error',
+  WARNING = 'Warning',
+  SUCCESS = 'Success'
 }
-function isEmpty(value) {
+function isEmpty(value: string | null) {
   return value == null || false || value === ''
 }
 
-function getTitle(strTitle, logLevel) {
+function getTitle(strTitle: string | null, logLevel: LOG_LEVEL) {
   return isEmpty(strTitle) ? logLevel : strTitle
 }
 
-function getContent(strTitle, strContent) {
+function getContent(strTitle: any, strContent: string | null) {
   return isEmpty(strContent) ? strTitle : strContent
 }
 
-function prettyPrint(title, text, color) {
+function prettyPrint(title: string | null, text: any, color: string) {
   console.log(
     `%c ${title} %c ${text} %c`,
     `background:${color};border:1px solid ${color}; padding: 1px; border-radius: 2px 0 0 2px; color: #fff;`,
@@ -24,25 +24,25 @@ function prettyPrint(title, text, color) {
     'background:transparent'
   )
 }
-function info(textOrTitle, content = '') {
+function info(textOrTitle: string | null, content = '') {
   const title = getTitle(textOrTitle, LOG_LEVEL.INFO)
   const text = getContent(textOrTitle, content)
   prettyPrint(title, text, '#909399')
 }
 
-function error(textOrTitle, content = '') {
+function error(textOrTitle: string | null, content = '') {
   const title = getTitle(textOrTitle, LOG_LEVEL.ERROR)
   const text = getContent(textOrTitle, content)
   prettyPrint(title, text, '#F56C6C')
 }
 
-function warning(textOrTitle, content = '') {
+function warning(textOrTitle: string | null, content = '') {
   const title = getTitle(textOrTitle, LOG_LEVEL.WARNING)
   const text = getContent(textOrTitle, content)
   prettyPrint(title, text, '#E6A23C')
 }
 
-function success(textOrTitle, content = '') {
+function success(textOrTitle: string | null, content = '') {
   const title = getTitle(textOrTitle, LOG_LEVEL.SUCCESS)
   const text = getContent(textOrTitle, content)
   prettyPrint(title, text, '#67C23A')
